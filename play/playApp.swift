b0d4d4e956848270e2910d9fd4a95ac8b7fe98e0
dhruvd-grappus <dhruv.devasthale@grappus.com>
2023-10-26 10:45:16 +0530

    Initial Commit

---
+++ play/playApp.swift
@@ -0,0 +1,17 @@
+//
+//  playApp.swift
+//  play
+//
+//  Created by Dhruv on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct playApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
